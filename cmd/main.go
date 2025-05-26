ffba7450b50716c6eed3f31662ea6db3f8333828
LeRaphouu <raphael.bonnet@ynov.com>
2025-05-26 16:58:15 +0200

    feat(main.go): relier la DB avec go

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	dsn := "root:Youyou3000.@tcp(127.0.0.1:3306)/forum"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connexion réussie à la base ✅")
+}
