6eca2dfd7455e45677c4f2808fddf15c37823efe
LeRaphouu <raphael.bonnet@ynov.com>
2025-06-02 15:16:22 +0200

    feat(go,html): cokkie added

--- Handler/index.go
+++ Handler/index.go
@@ -7,14 +7,27 @@ import (
 	"path/filepath"
 )
 
+type IndexPageData struct {
+	Username string
+}
+
 func ServeIndexPage(w http.ResponseWriter, r *http.Request) {
 	templatePath := filepath.Join("./templates/index.gohtml")
 	fmt.Println("Tentative de charger le template:", templatePath)
+	username := "Utilisateur"
+
+	cookie, err := r.Cookie("username")
+	if err == nil {
+		username = cookie.Value
+	}
+
+	data := IndexPageData{Username: username}
 
 	tmpl, err := template.ParseFiles(templatePath)
 	if err != nil {
 		http.Error(w, "Erreur de template: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w, nil)
+
+	tmpl.Execute(w, data)
 }
--- Handler/login.go
+++ Handler/login.go
@@ -64,4 +64,15 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	// Authentification réussie
 	fmt.Println("Connexion réussie pour", username)
 	http.Redirect(w, r, "/index", http.StatusSeeOther)
+
+	cookie := http.Cookie{
+		Name:     "username",
+		Value:    username,
+		Path:     "/",
+		MaxAge:   3600, // 1 heure
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/index", http.StatusSeeOther)
 }
