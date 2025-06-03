a88794e4ec18e1113cf6f7e2f31b7d851b1c4a62
clement alsina <clement.alsina@ynov.com>
2025-06-03 16:56:22 +0200

    debug

--- auth/auth.go
+++ auth/auth.go
@@ -14,8 +14,8 @@ import (
 
 // https://console.cloud.google.com/auth/overview for connect ID User
 var googleOAuthConfig = &oauth2.Config{
-	ClientID:     "275313117062-i0lb0eeq6lb5dhu5a0ieqvk0dokbe8h0.apps.googleusercontent.com",
-	ClientSecret: "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",
+	ClientID:     "GOOGLE_CLIENT_ID",
+	ClientSecret: "GOOGLE_CLIENT_SECRET",
 	RedirectURL:  "http://localhost:8080/auth/google/callback",
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 	Endpoint:     google.Endpoint,
