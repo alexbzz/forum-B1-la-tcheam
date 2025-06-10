922c2370d7f1086a5691935e405a63908ed18bb7
alexb <alexandre.bozzi@ynov.com>
2025-06-10 14:26:15 +0200

    feat complexity account password for register

--- Handler/register.go
+++ Handler/register.go
@@ -6,6 +6,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 var db *sql.DB
@@ -15,8 +17,7 @@ func InitDB(database *sql.DB) {
 }
 
 func ServeRegisterPage(w http.ResponseWriter, r *http.Request) {
-
-	templatePath := "./forum-B1-la-tcheam/templates/register.html"
+	templatePath := "./templates/register.html"
 
 	tmpl, err := template.ParseFiles(templatePath)
 	if err != nil {
@@ -26,18 +27,49 @@ func ServeRegisterPage(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func PasswordComplexity(pwd string) error {
+	if len(pwd) < 8 {
+		return fmt.Errorf("le mot de passe doit contenir au moins 8 caractères")
+	}
+
+	maj := regexp.MustCompile(`[A-Z]`)
+	min := regexp.MustCompile(`[a-z]`)
+	num := regexp.MustCompile(`[0-9]`)
+	spec := regexp.MustCompile(`[!@#~$%^&*()+|_.,<>?/\\-]`)
+
+	if !maj.MatchString(pwd) {
+		return fmt.Errorf("le mot de passe doit contenir au moins une lettre majuscule")
+	}
+	if !min.MatchString(pwd) {
+		return fmt.Errorf("le mot de passe doit contenir au moins une lettre minuscule")
+	}
+	if !num.MatchString(pwd) {
+		return fmt.Errorf("le mot de passe doit contenir au moins un chiffre")
+	}
+	if !spec.MatchString(pwd) {
+		return fmt.Errorf("le mot de passe doit contenir au moins un caractère spécial")
+	}
+	return nil
+}
+
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
 		return
 	}
 
-	username := r.FormValue("username")
-	email := r.FormValue("email")
+	username := strings.TrimSpace(r.FormValue("username"))
+	email := strings.TrimSpace(r.FormValue("email"))
 	password := r.FormValue("password")
 
 	if username == "" || email == "" || password == "" {
 		http.Error(w, "Champs requis manquants", http.StatusBadRequest)
+		return
+	}
+
+	// Validation complexité mot de passe
+	if err := validatePasswordComplexity(password); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -49,18 +81,15 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 
 	_, err = db.Exec("INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)", username, email, hash)
 	if err != nil {
-		fmt.Println("Erreur lors de l'insertion en base:", err)
-		if err.Error() != "" && (contains(err.Error(), "Duplicate") || contains(err.Error(), "unique")) {
+		errMsg := err.Error()
+		if strings.Contains(strings.ToLower(errMsg), "duplicate") || strings.Contains(strings.ToLower(errMsg), "unique") {
 			http.Error(w, "Cet utilisateur ou cet email existe déjà", http.StatusConflict)
 			return
 		}
+		fmt.Println("Erreur lors de l'insertion en base:", err)
 		http.Error(w, "Erreur d'insertion en base", http.StatusInternalServerError)
 		return
 	}
 
 	http.Redirect(w, r, "/index", http.StatusSeeOther)
-}
-
-func contains(s, substr string) bool {
-	return s != "" && (len(s) >= len(substr)) && (s[:len(substr)] == substr || s[len(s)-len(substr):] == substr || s[1:len(s)-1] != "" && contains(s[1:len(s)-1], substr))
 }
